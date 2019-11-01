760824e671ee8b9f3d37b5c1b2bc16fa72a95a32
Febiola Agatha <febiolagatha@gmail.com>
2019-11-01 20:32:37 +0700

    latihan pertemuan 8

---
+++ Pertemuan 8/latihan8.2.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <title>Koneksi Database MySQL</title>
+</head>
+<body>
+<h1>Koneksi database dengan mysql_fetch_array</h1>
+<?php
+$conn=mysqli_connect ("localhost","root","")
+      or die ("koneksi gagal");
+mysqli_select_db ($conn,"latihanpabw");
+$hasil=mysqli_query($conn,"select * from liga");
+while ($row=mysqli_fetch_array($hasil)) {
+    echo " liga " .$row["negara"];
+    echo " mempunyai " .$row[2];
+    echo " wakil di liga champion <br>";
+}
+?>
+</body>
+</html>
